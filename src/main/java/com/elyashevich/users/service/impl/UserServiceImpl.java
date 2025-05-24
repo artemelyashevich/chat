89f1d4155aed440aff80d7350d501b7aa123e247
artemelyashevich <elyashevich.artem06@gmail.com>
2025-05-24 22:42:28 +0300

    Add: user service

--- src/main/java/com/elyashevich/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/elyashevich/users/service/impl/UserServiceImpl.java
@@ -1,26 +1,63 @@
 package com.elyashevich.users.service.impl;
 
+import com.elyashevich.users.domain.entity.Role;
 import com.elyashevich.users.domain.entity.User;
+import com.elyashevich.users.exception.ResourceAlreadyExistsException;
+import com.elyashevich.users.exception.ResourceNotFoundException;
 import com.elyashevich.users.repository.UserRepository;
+import com.elyashevich.users.service.RoleService;
 import com.elyashevich.users.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String USER_WITH_EMAIL_WAS_NOT_FOUND_TEMPLATE = "User with email: '%s' was not found";
     private final UserRepository userRepository;
+    private final RoleService roleService;
 
     @Override
+    @Transactional
     public User save(User user) {
-        return null;
+        log.debug("Attempting create new user: {}", user);
+
+        if (this.userRepository.existsByEmail(user.getEmail())) {
+            String message = "User with email: '%s' already exists".formatted(user.getEmail());
+            log.info(message);
+            throw new ResourceAlreadyExistsException(message);
+        }
+
+        Role role = this.roleService.findByName(ROLE_USER);
+
+        user.setRoles(Set.of(role));
+
+        User newUser = this.userRepository.save(user);
+
+        log.info("User created: {}", newUser);
+        return newUser;
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        log.debug("Attempting fin user by email: '{}'", email);
+
+        User user = this.userRepository.findByEmail(email).orElseThrow(
+                () -> {
+                    String message = USER_WITH_EMAIL_WAS_NOT_FOUND_TEMPLATE.formatted(email);
+                    log.info(message);
+                    return new ResourceNotFoundException(message);
+                }
+        );
+
+        log.info("User found: {}", user);
+        return user;
     }
 }
